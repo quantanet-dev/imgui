b543dca36a7852a5769c7bca98a1fd0eada63402
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-13 01:56:01 -0500

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+project "imgui"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "On"
+	warnings "Off"
+
+	targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+	targetdir ("bin-obj/" .. outputDir .. "/%{prj.name}")
+
+	files {
+		"backends/imgui_impl_glfw.h",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_opengl3_loader.h",
+		"*.h",
+		"*.cpp",
+	}
+
+	includedirs {
+		"./",
+		"%{wks.location}/Helio/vendor/glfw/include"
+	}
